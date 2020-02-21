ad8a61977f8c3d6aea0573184ea66a76ed227eeb
Ruslan <35609502+RuslanKot@users.noreply.github.com>
2020-02-21 21:29:59 +0200

    +

---
+++ html5/lesson11/scripts/main.js
@@ -0,0 +1,14 @@
+function startLogic() {
+
+    var newHtml = '<em>Old Header</em>';
+
+    document.getElementById('headerItem').innerHTML = index.html;
+
+}
+startLogic();
+
+function clickHandler() {
+
+    alert('You clicked me!');
+
+}
