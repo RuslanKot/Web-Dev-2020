e85dc51ee975989e7b739eaf2562b102ba078f13
Ruslan <35609502+RuslanKot@users.noreply.github.com>
2020-03-10 21:24:21 +0200

    +

---
+++ html51/Practic Java/js/main.js
@@ -0,0 +1,10 @@
+function Run() {
+    var a = document.getElementById('a')
+    var b = document.getElementById('b')
+    var res = document.getElementById('res')
+
+    var sum = a + b;
+    var b = 2;
+    var res = a + b;
+    res.innerHtml = "Result"
+}
