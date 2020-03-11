27029f7b224a93f1c2bcbe618f1066f642f95e86
Ruslan <35609502+RuslanKot@users.noreply.github.com>
2020-03-11 22:35:54 +0300

    crash

--- html51/Practic Java/js/main.js
+++ html51/Practic Java/js/main.js
@@ -1,10 +1,18 @@
 function Run() {
-    var a = document.getElementById('a')
-    var b = document.getElementById('b')
-    var res = document.getElementById('res')
-
+    var a = document.getElementById('a').value | 0;
+    var b = document.getElementById('b').value | 0;
+    var res = document.getElementById('res');
     var sum = a + b;
-    var b = 2;
-    var res = a + b;
-    res.innerHtml = "Result"
+    var rizn = a - b;
+    var dob = a * b;
+    res.innerHTML = "Результат:" + "<br>" + "<hr>" + "Сума = " + sum + "<br>" + "Різниця = " + rizn + "<br>" + "Добуток = " + dob;
+}
+
+function Run2() {
+    var x = document.getElementById('x').value | 0;
+    var y = document.getElementById('y').value | 0;
+    var res = document.getElementById('res02');
+    var sum = (Math.abs(x) - Math.abs(y)) / (1 + Math.abs(x) * Math.abs(y));
+    res.innerHTML = "Результат" + "<br>" + sum;
+
 }
