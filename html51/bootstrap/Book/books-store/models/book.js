411af582dd528cbf1ee5ce777f94be7299f090cb
Ruslan <35609502+RuslanKot@users.noreply.github.com>
2020-04-21 00:45:49 +0300

    +

--- html51/bootstrap/Book/books-store/models/book.js
+++
@@ -1,8 +0,0 @@
-class book {
-    constructor() {
-        this.id = 0;
-        this.name = "";
-        this.year = 1;
-    }
-}
-module.exports = book;
--- html51/bootstrap/Book/books-store/routes/index.js
+++ html51/bootstrap/Book/books-store/routes/index.js
@@ -1,15 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var book = require('../models/book')
-    /* GET home page. */
+
+/* GET home page. */
 router.get('/', function(req, res, next) {
-    let books = [];
-    let count = 0;
-    while (count < 5) {
-        books.push(new book)
-        count++;
-    }
-    res.render('index', { title: 'Express', books: books });
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
